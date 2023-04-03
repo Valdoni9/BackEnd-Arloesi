be71053b938bd587cc89c6933abd6688c1f0761f
Antonio Valdoni <antoniovaldoni@gmail.com>
2023-04-02 23:37:50 -0400

    remove

--- src/app/todo/dto/update-todo.dto.ts
+++ src/app/todo/dto/update-todo.dto.ts
@@ -1,5 +1,3 @@
-import { Validate } from 'class-validator';
 import { CreateTodoDto } from './create-todo.dto';
-import { forbiddentochange } from './validate-todo.dto';
 
 export class UpdateTodoDto extends CreateTodoDto {}
